d7a96313df3912fe8be5f642ffe32dd25ac98b54
Chirayu <chirayu.chomsri@gmail.com>
2022-06-02 00:44:14 +0700

    feat: Paginator Data Table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 
@@ -44,7 +45,8 @@ import { MatSortModule } from '@angular/material/sort';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatSortModule
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/tasks/tasks-home/tasks-home.component.ts
+++ src/app/components/tasks/tasks-home/tasks-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Task } from 'src/app/models/product.model';
@@ -17,16 +18,48 @@ export class TasksHomeComponent implements OnInit {
   textSearch?: string;
 
   @ViewChild(MatSort, { static: true }) sort!: MatSort
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator
 
     constructor() { }
 
   ngOnInit(): void {
     this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
     this.feedData();
   }
 
   feedData() {
     const dummy: Task[] = [
+      {
+        name: 'นายวณัฐ เกิดศุภชัย',
+        date: '05/19/2022',
+        status: 'A0',
+        image: 'https://www.art-tech.co.th/wp-content/uploads/2019/04/606bannerhome.png'
+      },
+      {
+        name: 'นายบุญช่วย บัวผุด',
+        date: '04/29/2022',
+        status: 'A1',
+        image: 'https://www.art-tech.co.th/wp-content/uploads/2019/04/606bannerhome.png'
+      },
+      {
+        name: 'อบต.กระแสบน(สายเขาผักกูด-เขาวงเวียน)',
+        date: '05/10/2022',
+        status: 'A1',
+        image: 'https://www.art-tech.co.th/wp-content/uploads/2019/04/606bannerhome.png'
+      },
+      {
+        name: 'นายประเวศ มงคลคำ',
+        date: '03/10/2022',
+        status: 'A2',
+        image: 'https://www.art-tech.co.th/wp-content/uploads/2019/04/606bannerhome.png'
+      },
+      {
+        name: 'อบต.ทุ่งควายกิน(ซอยขวัญเมือง)',
+        date: '05/05/2022',
+        status: 'B1',
+        image: 'https://www.art-tech.co.th/wp-content/uploads/2019/04/606bannerhome.png'
+      },
       {
         name: 'นายวณัฐ เกิดศุภชัย',
         date: '05/19/2022',
